df24148ae702c30bb9b72317d82f138f3ecb8755
Echo3A3B <echo3a3b@naver.com>
2024-01-04 00:33:23 +0900

    Update login.c

--- 240103/login/login/login.c
+++ 240103/login/login/login.c
@@ -1,20 +1,40 @@
 #include<stdio.h>
 
-void check();
+int check(id, pw);
 
 int main() {
 	int id;
 	int pw;
-	int count= 0;
+	
 	while (1) {
 		printf("id: ____\b\b\b\b");
 		scanf_s("%d", &id);
 		printf("pw: ____\b\b\b\b");
 		scanf_s("%d", &pw);
+		if (check(id, pw)) {
+			break;
+		}
 	}
+	printf("로그인 성공\n");
+
+	return 0;
 }
 
-void check() {
-	printf("횟수 초과");
-	exit(1);
+int check(id, pw) {
+	static int identy = 1234;
+	static int password = 5678;
+	static int count = 0;
+
+	if (id == identy && pw == password)
+		return 1;
+	else {
+		count++;
+		if (count >= 3) {
+			printf("횟수 초과\n");
+			exit(1);
+		}
+		return 0;
+	}
+		
+	
 }
